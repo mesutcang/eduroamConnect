d28bb6084b2a50d7e35c221d684c082b37b9bffb
Your Name <you@example.com>
2012-08-19 00:15:12 +0300

    new interface and implementations

--- src/com/eduroam/Interface.java
+++ src/com/eduroam/Interface.java
@@ -6,6 +6,8 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
@@ -17,6 +19,8 @@ import android.app.AlertDialog;
 import android.app.TabActivity;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
@@ -33,6 +37,10 @@ import android.widget.TabHost.TabSpec;
 import android.widget.Toast;
 
 public class Interface extends Activity implements OnClickListener {
+	 XmlParser xml;
+	   ConfigurationRegularExpressionFieldParser parse = new ConfigurationRegularExpressionFieldParser();
+	  
+	private WifiManager wifi;
 	public static final Pattern WEB_URL_PATTERN
     = Pattern.compile(
         "((?:(http|https|Http|Https):\\/\\/(?:(?:[a-zA-Z0-9\\$\\-\\_\\.\\+\\!\\*\\'\\(\\)"
@@ -78,7 +86,7 @@ public class Interface extends Activity implements OnClickListener {
                         // matching as foo.su
 	Options options = new Options();
 	Button btnDownloadConfiguration,btnConnect;
-	EditText input;
+	EditText input,etUsername,etPassword;
 	File file;
 	
 	
@@ -87,6 +95,8 @@ public class Interface extends Activity implements OnClickListener {
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.view);
 	 
+	    wifi = (WifiManager)getSystemService(WIFI_SERVICE); 
+	    
 	    btnDownloadConfiguration = (Button) findViewById(R.id.btnDownloadConfiguration);
 	    btnDownloadConfiguration.setOnClickListener(this);
 	    
@@ -95,6 +105,11 @@ public class Interface extends Activity implements OnClickListener {
 	    
 	    input=new EditText(this);
 	    input.setText(options.getDefaultConfigurationURL());
+	    
+	    
+	    etUsername = (EditText) findViewById(R.id.etUsername);
+	    
+	    etPassword = (EditText) findViewById(R.id.etPassword);
 	   
 	}
 	@Override
@@ -156,17 +171,240 @@ public void onClick(View v) {
             new File(dir, children[i]).delete();
         }
     }
-	*/
+	
 	
 	File f = new File(Environment.getExternalStorageDirectory().getAbsolutePath()+File.separatorChar+options.getStorageDirectory());
 	File[] files = f.listFiles();
 	for (File inFile : files) {
 	    Toast.makeText(this, inFile.getName(), Toast.LENGTH_LONG).show();
 	}
+	*/
+	if (file== null || !file.isFile()) {
+		Toast.makeText(this, "Please define a valid configuration file.", Toast.LENGTH_LONG).show();
+		return;
+		
+		
+	}
+	
+	if (xml ==null) {
+		xml = new XmlParser(getResources());
+	}
+	configure();
+	
+	
+	
 
 }
 }
 
+	private void configure() {
+		ConfigurationRegularExpressionFieldParser parse = new ConfigurationRegularExpressionFieldParser();
+		Options options = new Options();
+		
+		String conType = parse.getEncryptionType(xml);
+		
+		String eapType = options.getAcceptEAPTypeDefinition( parse.getAcceptEAPTypes(xml));
+		
+		if (eapType.equalsIgnoreCase("PEAP")) {
+			
+			try {
+				String ssid = parse.getPayloadContentSSID_STR(xml);
+				String hiddenSSID = parse.getHiddenNetwork(xml);
+				String userName = parse.getUsernameField(xml);
+				String userPassword = parse.getUserPassword(xml);
+				
+				if (!etUsername.getText().toString().trim().equals("")) {
+					userName = etUsername.getText().toString().trim();
+				}
+				
+				if (!etPassword.getText().toString().trim().equals("")) {
+					userPassword = etPassword.getText().toString().trim();
+				}
+				connectDot1X(ssid,hiddenSSID,userName,userPassword,eapType);
+				
+				
+			} catch (Exception e) {
+				Toast.makeText(this, "error:" + e.getMessage(), Toast.LENGTH_LONG).show();
+			}
+			
+		}else if (conType.equalsIgnoreCase("WPA")) {
+			try {
+				String ssid = parse.getPayloadContentSSID_STR(xml);
+				String hiddenSSID = parse.getHiddenNetwork(xml);
+				String password = parse.getPayloadContentPassword(xml);
+				connectWPA(ssid, Boolean.valueOf(hiddenSSID), password);
+				
+				//Toast.makeText(this, password, Toast.LENGTH_LONG).show();
+			} catch (Exception e) {
+				Toast.makeText(this, "error:" + e.getMessage(), Toast.LENGTH_LONG).show();
+			}
+			
+		}else if (conType.equalsIgnoreCase("WEP")) {
+			String ssid = parse.getPayloadContentSSID_STR(xml);
+			String hiddenSSID = parse.getHiddenNetwork(xml);
+			String password = parse.getPayloadContentPassword(xml);
+			connectWEP(ssid, Boolean.valueOf(hiddenSSID), password);
+		}
+		
+	
+}
+	
+	private void connectDot1X(String ssid2, String hiddenSSID, String userName2, String userPassword, String eapType) {
+		final String INT_PRIVATE_KEY = "private_key";
+	    final String INT_PHASE2 = "phase2";
+	    final String INT_PASSWORD = "password";
+	    final String INT_IDENTITY = "identity";
+	    final String INT_EAP = "eap";
+	    final String INT_CLIENT_CERT = "client_cert";
+	    final String INT_CA_CERT = "ca_cert";
+	    final String INT_ANONYMOUS_IDENTITY = "anonymous_identity";
+	    final String INT_ENTERPRISEFIELD_NAME = "android.net.wifi.WifiConfiguration$EnterpriseField";
+	    
+	    //user credentials and ssid
+	    String userName = userName2;
+	    String password = userPassword; //password maked.
+	    String ssid =  ssid2;
+	    
+	    
+	    //connection properties
+	    final String ENTERPRISE_EAP = eapType;
+        final String ENTERPRISE_CLIENT_CERT = null;
+        final String ENTERPRISE_PRIV_KEY = null;        
+        
+        //final String ENTERPRISE_PHASE2 = "auth=MSCHAP";
+        final String ENTERPRISE_PHASE2 = "";
+        final String ENTERPRISE_ANON_IDENT = null;
+        
+        
+        
+        WifiConfiguration selectedConfig = new WifiConfiguration();
+        
+        selectedConfig.SSID = "\""+ssid+"\"";
+        selectedConfig.priority = 48;
+        selectedConfig.hiddenSSID = false;
+
+        selectedConfig.allowedKeyManagement.clear();
+        selectedConfig.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.IEEE8021X);
+        selectedConfig.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.WPA_EAP);
+
+        
+        selectedConfig.allowedGroupCiphers.clear();
+        selectedConfig.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.CCMP);
+        selectedConfig.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.TKIP);
+        selectedConfig.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.WEP104);
+        selectedConfig.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.WEP40);
+
+        
+        selectedConfig.allowedPairwiseCiphers.clear();
+        selectedConfig.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.CCMP);
+        selectedConfig.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.TKIP);
+
+        
+        selectedConfig.allowedProtocols.clear();
+        selectedConfig.allowedProtocols.set(WifiConfiguration.Protocol.RSN);
+        selectedConfig.allowedProtocols.set(WifiConfiguration.Protocol.WPA);
+
+        try {
+            Class[] wcClasses = WifiConfiguration.class.getClasses();
+            Class wcEnterpriseField = null;
+
+            for (Class wcClass : wcClasses)
+                if (wcClass.getName().equals(INT_ENTERPRISEFIELD_NAME)) 
+                {
+                    wcEnterpriseField = wcClass;
+                    break;
+                }
+            
+            boolean noEnterpriseFieldType = false; 
+            
+            if(wcEnterpriseField == null)
+                noEnterpriseFieldType = true; // 
+
+            Field wcefAnonymousId = null, wcefCaCert = null, wcefClientCert = null, wcefEap = null, wcefIdentity = null, wcefPassword = null, wcefPhase2 = null, wcefPrivateKey = null;
+            Field[] wcefFields = WifiConfiguration.class.getFields();
+            
+            //Get fields from hidden api
+            for (Field wcefField : wcefFields) 
+            {
+                if (wcefField.getName().equals(INT_ANONYMOUS_IDENTITY))
+                    wcefAnonymousId = wcefField;
+                else if (wcefField.getName().equals(INT_CA_CERT))
+                    wcefCaCert = wcefField;
+                else if (wcefField.getName().equals(INT_CLIENT_CERT))
+                    wcefClientCert = wcefField;
+                else if (wcefField.getName().equals(INT_EAP))
+                    wcefEap = wcefField;
+                else if (wcefField.getName().equals(INT_IDENTITY))
+                    wcefIdentity = wcefField;
+                else if (wcefField.getName().equals(INT_PASSWORD))
+                    wcefPassword = wcefField;
+                else if (wcefField.getName().equals(INT_PHASE2))
+                    wcefPhase2 = wcefField;
+                else if (wcefField.getName().equals(INT_PRIVATE_KEY))
+                    wcefPrivateKey = wcefField;
+            }
+
+
+            Method wcefSetValue = null;
+            if(!noEnterpriseFieldType)
+            {
+            for(Method m: wcEnterpriseField.getMethods())
+                if(m.getName().trim().equals("setValue"))
+                    wcefSetValue = m;
+            }
+
+
+            
+            if(!noEnterpriseFieldType)
+                wcefSetValue.invoke(wcefEap.get(selectedConfig), ENTERPRISE_EAP);
+
+            
+            if(!noEnterpriseFieldType)
+            	wcefSetValue.invoke(wcefPhase2.get(selectedConfig), ENTERPRISE_PHASE2);
+
+            if(!noEnterpriseFieldType)
+                wcefSetValue.invoke(wcefAnonymousId.get(selectedConfig), ENTERPRISE_ANON_IDENT);
+
+            if(!noEnterpriseFieldType)
+                wcefSetValue.invoke(wcefPrivateKey.get(selectedConfig), ENTERPRISE_PRIV_KEY);
+
+            if(!noEnterpriseFieldType)
+                wcefSetValue.invoke(wcefIdentity.get(selectedConfig), userName);
+
+            if(!noEnterpriseFieldType)
+                wcefSetValue.invoke(wcefPassword.get(selectedConfig), password);
+
+            if(!noEnterpriseFieldType)
+                wcefSetValue.invoke(wcefClientCert.get(selectedConfig), ENTERPRISE_CLIENT_CERT);
+
+            try{
+            Field wcAdhoc = WifiConfiguration.class.getField("adhocSSID");
+            Field wcAdhocFreq = WifiConfiguration.class.getField("frequency");
+
+            wcAdhoc.setBoolean(selectedConfig, false);
+            int freq = 2462;  
+            wcAdhocFreq.setInt(selectedConfig, freq); 
+            } catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        //Now we can add eduroam profile.
+        int ret = wifi.addNetwork(selectedConfig);
+        
+        boolean a = wifi.enableNetwork(ret, false);   
+        
+        boolean b = wifi.saveConfiguration();
+        
+        boolean c = wifi.enableNetwork(ret, true);   
+        
+        
+		
+	}
 	/*
 	 * Downloads specified file from url.
 	 */
@@ -239,4 +477,82 @@ public void onClick(View v) {
 		
 		
 	}
+private void connectWPA(String ssid, Boolean hiddenSsid,String preSharedKey){
+		
+		
+		try {
+	     WifiManager wifi = (WifiManager)getSystemService(WIFI_SERVICE);
+	     WifiConfiguration wc = new WifiConfiguration(); 
+	     wc.SSID = "\"" + ssid + "\""; //IMP! This should be in Quotes!!
+	     wc.hiddenSSID = hiddenSsid;
+	     wc.status = WifiConfiguration.Status.ENABLED;     
+	     wc.priority = 40;
+	     wc.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.WPA_PSK);
+	     wc.allowedProtocols.set(WifiConfiguration.Protocol.WPA);
+
+	     wc.allowedAuthAlgorithms.set(WifiConfiguration.AuthAlgorithm.OPEN);
+
+	     wc.preSharedKey = "\"" + preSharedKey + "\"";// 
+
+	     Log.d("ssid : ", wc.SSID );
+
+	    
+	     
+	      int ret = wifi.addNetwork(wc);
+	        
+	        boolean a = wifi.enableNetwork(ret, false);   
+	        
+	        boolean b = wifi.saveConfiguration();
+	        
+	        boolean c = wifi.enableNetwork(ret, true);
+		}catch (Exception e) {
+				Toast.makeText(this, "error:" + e.getMessage(), Toast.LENGTH_LONG).show();
+			}
+
+	}
+	
+	private void connectWEP(String ssid, Boolean hiddenSsid,String preSharedKey){
+		
+		
+		try {
+			
+		   
+			
+	     WifiManager wifi = (WifiManager)getSystemService(WIFI_SERVICE);
+	     WifiConfiguration wc = new WifiConfiguration(); 
+	     wc.SSID = "\"" + ssid + "\""; //IMP! This should be in Quotes!!
+	     wc.hiddenSSID = hiddenSsid;
+	     wc.status = WifiConfiguration.Status.ENABLED;     
+	     wc.priority = 40;
+	     wc.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
+	     wc.allowedProtocols.set(WifiConfiguration.Protocol.RSN); 
+  	    wc.allowedProtocols.set(WifiConfiguration.Protocol.WPA);
+  	    wc.allowedAuthAlgorithms.set(WifiConfiguration.AuthAlgorithm.OPEN);
+  	    wc.allowedAuthAlgorithms.set(WifiConfiguration.AuthAlgorithm.SHARED);
+  	    wc.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.CCMP);
+  	    wc.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.TKIP);
+  	    wc.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.WEP40);
+  	    wc.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.WEP104);
+         wc.allowedAuthAlgorithms.set(WifiConfiguration.AuthAlgorithm.OPEN);
+
+	     wc.wepKeys[0] = "\"" + preSharedKey + "\"";// 
+	     wc.wepTxKeyIndex = 0;
+
+	     Log.d("ssid : ", wc.SSID );
+
+	    
+	     
+	      int ret = wifi.addNetwork(wc);
+	        
+	        boolean a = wifi.enableNetwork(ret, false);   
+	        
+	        boolean b = wifi.saveConfiguration();
+	        
+	        boolean c = wifi.enableNetwork(ret, true);
+		}catch (Exception e) {
+				Toast.makeText(this, "error:" + e.getMessage(), Toast.LENGTH_LONG).show();
+			}
+
+	}
+	
 }
